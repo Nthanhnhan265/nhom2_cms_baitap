91afcdb34b8320008e5fc8d60166e8efb136a88b
TinhHaF <haquoctinh3005@gmail.com>
2024-10-06 11:35:47 +0700

    add filter hooks, body_class

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -4,3 +4,11 @@ function load_my_style(){
     wp_enqueue_style('my_style',get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts','load_my_style');
+
+function wp_custom_body_class( $classes ) {
+    if ( is_single() ) {
+        $classes[] = 'single-post-view';
+    }
+    return $classes;
+}
+add_filter( 'body_class', 'wp_custom_body_class' );
